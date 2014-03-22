085f8a42df0c089623c52ad10ff7be0fc2c3648e
Devran Ünal <devran@me.com>
2014-03-22 14:16:51 +0100

    Add labels and views to news cell

--- NewsSightseeing-iOS/app/views/news_view_cell.rb
+++ NewsSightseeing-iOS/app/views/news_view_cell.rb
@@ -1,19 +1,46 @@
 class NewsViewCell < UICollectionViewCell
+  attr_accessor :backgroundView
   attr_accessor :heroView
+  attr_accessor :shadowFromTop
+  attr_accessor :headlineLabel
+  
   def initWithFrame(frame)
     super
     
+    self.backgroundView = UIView.alloc.initWithFrame(CGRectZero).tap do |background|
+      background.clipsToBounds       = true
+      background.layer.cornerRadius  = 10
+      background.layer.masksToBounds = true
+      self.addSubview(background)
+    end
+    
     self.heroView = UIImageView.alloc.initWithFrame(CGRectZero).tap do |imageView|
-      imageView.image = UIImage.imageNamed("Dummy-Images/News-Hero-1.jpg")
-      imageView.contentMode   = UIViewContentModeScaleAspectFill
-      imageView.clipsToBounds = true
-      self.addSubview(imageView)
+      imageView.image       = UIImage.imageNamed("Dummy-Images/News-Hero-#{rand(5)}.jpg")
+      imageView.contentMode = UIViewContentModeScaleAspectFill
+      self.backgroundView.addSubview(imageView)
+    end
+    
+    self.shadowFromTop = UIImageView.alloc.initWithFrame(CGRectZero).tap do |imageView|
+      imageView.image       = UIImage.imageNamed("News-Shadow-From-Top.png")
+      imageView.alpha       = 0.7
+      imageView.contentMode = UIViewContentModeScaleToFill
+      self.backgroundView.addSubview(imageView)
+    end
+    
+    self.headlineLabel = UILabel.alloc.init.tap do |label|
+      label.textColor               = UIColor.whiteColor
+      label.font                    = UIFont.boldSystemFontOfSize(16)
+      label.userInteractionEnabled  = false
+      self.addSubview(label)
     end
     
     self
   end
   
   def layoutSubviews
-    self.heroView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)
+    self.backgroundView.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)
+    self.heroView.frame       = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)
+    self.shadowFromTop.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height/2)
+    self.headlineLabel.frame  = CGRectMake(10, 10, self.frame.size.width-20-20, 20)
   end
 end
